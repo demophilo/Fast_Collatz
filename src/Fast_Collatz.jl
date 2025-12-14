991913b1e6888d3e8d333425840257704f4d743d
Karl Linek <karl@linek.at>
2025-12-14 07:12:20 +0100

    struct Pow2Int added

--- src/Fast_Collatz.jl
+++ src/Fast_Collatz.jl
@@ -1,5 +1,18 @@
 module Fast_Collatz
 
-greet() = print("Hello World!")
+struct Pow2Int
+	mant::UInt128   # always odd
+	exp::UInt32
+
+	# Inner constructor enforces the invariant
+	function Pow2Int(m::UInt128, e::UInt32)
+		if iseven(m)
+			throw(ArgumentError("Mantissa must be odd!"))
+		end
+		new(m, e)
+	end
+end
+
+
 
 end # module Fast_Collatz
